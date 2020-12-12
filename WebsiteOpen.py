09e20d2c9b98b4963d2209f83323107fd45838d7
Rupam Paul <rupam.paul4@gmail.com>
2020-12-12 14:40:56 +0530

    Changes made to WebsiteOpen.py

--- WebsiteOpen.py
+++ WebsiteOpen.py
@@ -1,4 +1,17 @@
-import webbrowser as wb
+from urllib.request import urlopen
 
-url='https://www.youtube.com/'
-wb.open(url)
+url="https://fantasy.premierleague.com/statistics"
+
+page=urlopen(url)
+
+html_bytes=page.read()
+html=html_bytes.decode("utf-8")
+
+title_index=html.find("<title>")
+
+start_index=title_index + len("<title>")
+end_index = html.find("</title>")
+
+title=html[start_index:end_index]
+
+print(title)
