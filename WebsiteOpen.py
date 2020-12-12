4d7485bda680f372dc1ff5c21b52d4bae6437fd0
Saurav <srvbhowmick530@gmail.com>
2020-12-12 17:52:32 +0530

    executed now

--- WebsiteOpen.py
+++ WebsiteOpen.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 
 url="https://fantasy.premierleague.com/statistics"
 #hellowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+#jabababaaba
 page=urlopen(url)
 
 html_bytes=page.read()
